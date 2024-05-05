0b2b3dd32477dcb6cfbdd002f6f4aa39dcebbf07
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-05 23:24:43 +0300

    new files

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
+"""
+basic flask app
+"""
 
-from flask import Flask
-from flask_babel import Babel
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
+    """
+    returns the render hello world
+    """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
