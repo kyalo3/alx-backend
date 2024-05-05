c0e3f2d0438ac2a02dff186c36b5ceec93d7ed90
Kyalo3 <kyalokimeu0@gmail.com>
2024-05-05 22:01:11 +0300

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-""" Simple helper function """
-
+"""Hypermedia pagination
+    """
 import csv
-import math
-from typing import List, Tuple, Dict, Any
+from typing import List, Dict, Any
+
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -25,51 +26,27 @@ class Server:
 
         return self.__dataset
 
-    def index_range(self, page: int, page_size: int) -> Tuple[int, int]:
-        """ returns a tuple """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        new_list = []
-        new_list.append((page_size * page) - page_size)
-        new_list.append(page_size * page)
-        return tuple(new_list)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ returns an empty list """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        self.dataset()
-        pages = self.index_range(page, page_size)
-        data = []
-        if pages[-1] > len(self.__dataset):
-            return []
-        for i in range(pages[0], pages[-1]):
-            data.append(self.__dataset[i])
-        return data
+        """get page
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-        """ get full data details"""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-        page_size = page_size
-        page = page
+        """get hypermedia pagination
+        """
         data = self.get_page(page, page_size)
-        if page > 1:
-            next_page = page + 1
-        else:
-            next_page = None
-        if page_size < (len(self.dataset()) // page_size):
-            prev_page = page - 1
-        else:
-            prev_page = None
-        total_pages = len(self.dataset()) // page_size
-
-        result = {
-            "page_size": page_size,
-            "page": page,
-            "data": data,
-            "next_page": next_page,
-            "prev_page": prev_page,
-            "total_pages": total_pages
+        total_pages = len(self.dataset()) / page_size
+        if len(self.dataset()) % page_size != 0:
+            total_pages += 1
+        return {
+            'page_size': page_size,
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 <= total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': int(total_pages)
         }
-        return result
